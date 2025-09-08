31e56f0fe0b3a43dc9c64622bb67dcec36b866e6
andreyoz1 <andrey.murilloo@gmail.com>
2025-09-08 18:10:18 -0500

    creacion de modelos

---
+++ taller_backend/Models/DetalleTipoPersona.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; //para mapear la clase con la tabla de la base de datos
+
+namespace taller_backend.Models
+{
+    public class DetalleTipoPersona
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+        [Column("descripcion")]
+        public string? Descripcion { get; set; }
+
+        // navegación (colección)
+        public ICollection<Persona> Personas { get; set; } = new List<Persona>();
+
+    }
+}
---
+++ taller_backend/Models/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; //para mapear la clase con la tabla de la base de datos
+
+namespace taller_backend.Models
+{
+    public class Persona
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+        [Column("nombre")]
+        public string? Nombre { get; set; }
+
+        [Column("telefono")]
+        public string? Telefono { get; set; }
+
+        [Column("idDetalleTipoPersona")]
+        public long? idDetalleTipoPersona { get; set; }
+
+        //fecha de creacion
+        [Column("fechaCreacion")]
+        public DateTime? fechaCreacion { get; set; }
+
+
+    }
+}
